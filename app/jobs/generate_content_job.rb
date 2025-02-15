ad2cf572b3546cf19fdd9f1e4cb47b237b6560c5
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-02-15 18:42:45 +0100

    gestion des erreurs de génération

--- app/jobs/generate_content_job.rb
+++ app/jobs/generate_content_job.rb
@@ -5,23 +5,19 @@ class GenerateContentJob < ApplicationJob
     @nich = nich
     client = OpenAI::Client.new
 
-     # Création ou mise à jour des modèles associés avec récupération immédiate
+# Création ou mise à jour des modèles associés avec récupération immédiate
 
 avatar = @nich.create_avatar!(description: generate_text(client, "Décris un avatar pour #{@nich.name}")) unless @nich.avatar
-
-
+puts 'avatar created'
 offer = @nich.create_offer!(answer: generate_text(client, "Crée une offre pour #{@nich.name}")) unless @nich.offer
-
-
-result = @nich.create_hypothesis_result!(answer: generate_text(client, "Quel résultat le client obtient avec #{@nich.name} ?")) unless @nich.result
-
+puts 'offer created'
 message = @nich.create_message!(answer: generate_text(client, "Quel message marketing utiliser pour #{@nich.name} ?")) unless @nich.message
-
-
+puts 'message created'
 script = @nich.create_script!(question: generate_text(client, "Rédige un script de vente pour #{@nich.name}")) unless @nich.script
-if script
-  Turbo::StreamsChannel.broadcast_replace_to("script_#{script.id}", target: "script_#{script.id}", partial: "scripts/show", locals: { script: script })
-end
+puts 'script created'
+result = @nich.create_hypothesis_result!(answer: generate_text(client, "Quel résultat le client obtient avec #{@nich.name} ?")) unless @nich.hypothesis_result
+puts 'results created'
+
 
     # Rediriger vers la page du projet une fois terminé
     redirect_to project_path(@nich.project), notice: "Contenu généré avec succès!"
